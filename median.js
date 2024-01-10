4d0d84632e39dabde39b5a2ca31b810f9f9cc820
AeternumIter <87672613+AeternumIter@users.noreply.github.com>
2024-01-10 14:44:17 -0500

    Create median.js

---
+++ median.js
@@ -0,0 +1,9 @@
+function median(x) {
+  var temp = x;
+  temp.sort(function(a,b){return a-b});
+  if (temp.length%2===1) {
+    return temp[(temp.length-1)/2];
+  } else {
+    return (temp[temp.length/2]+temp[temp.length/2-1])/2
+  }
+}
