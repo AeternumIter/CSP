8fe2324005336f18cd1e4f59a785a569c702627c
AeternumIter <87672613+AeternumIter@users.noreply.github.com>
2024-02-05 13:41:40 +0000

    updated stuff

--- openHTML.js
+++ openHTML.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
 
 http.createServer(function(req,res){
-    var q = url.parse(req.url,true);
-    var filename="."+q.pathname;
+    let q = url.parse(req.url,true);
+    let filename="."+q.pathname;
     fs.readFile(filename, function(err,data){
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
