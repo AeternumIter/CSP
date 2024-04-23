ea58739f3625ae54846aae72dbf25dd45e36811b
AeternumIter <87672613+AeternumIter@users.noreply.github.com>
2024-04-23 13:28:52 +0000

    binary sorting tiiime

--- binary.py
+++ binary.py
@@ -1,12 +1,22 @@
 import math
 numbers=[]
-notDone=True
 i=1
-for i in 1001:
+for i in range(1,1001):
     numbers.append(i)
-print("Choose a number between 1-1000")
-while(notDone):
-    print("Is it higher(h), lower(l), or equal(e) to "+numbers[math.floor(numbers.length/2)])
+print("Choose an integer between 1-1000")
+while(True):
+    try:
+        print("Is it higher(h), lower(l), or equal(e) to "+str(numbers[math.floor(len(numbers)/2)]))
+    except:
+        print("Your number doesn't exist :/")
+        quit()
     answer=input()
-    if answer=="h":
-        numbers=numbers[math.floor(numbers.length/2):numbers.length-1]
+    if answer=="e":
+        print("I got it!")
+        quit()
+    elif answer=="h":
+        numbers=numbers[math.floor(len(numbers)/2)+1:len(numbers)]
+    elif answer=="l":
+        numbers=numbers[0:math.floor(len(numbers)/2)]
+    else:
+        print("invalid response")
